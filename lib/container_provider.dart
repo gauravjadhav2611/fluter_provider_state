5096c099721a4ba4ec4fa2f993a42fdbd77e80a9
Gaurav <gauravjadhav2611gmail.com>
2025-07-07 23:24:29 +0530

    minor change

--- lib/container_provider.dart
+++ lib/container_provider.dart
@@ -1,19 +1,21 @@
 import 'package:flutter/material.dart';
 
 class ContainerProvider extends ChangeNotifier{
-  double containerHeight;
+  double containerHeight = 100.0;
 
   ContainerProvider({required this.containerHeight});
 
   increaseHeight() {
-    if(containerHeight !<400)
-    containerHeight += 10.0;
+    if(containerHeight !<400) {
+      containerHeight += 10.0;
+    }
     notifyListeners();
   }
 
   decreaseHeight(){
-    if(containerHeight !> 10.0)
-    containerHeight -= 10.0;
+    if(containerHeight !> 10.0) {
+      containerHeight -= 10.0;
+    }
     notifyListeners();
   }
 }
